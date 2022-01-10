90503d45c412b8577830b5a86b6fb6b8734af8e2
pjflanagan <pjflanagan1@gmail.com>
2022-01-09 23:51:20 -0500

    move to bit.io

--- host/db/times.ts
+++ host/db/times.ts
@@ -1,12 +1,15 @@
 
 import { Client } from 'pg';
+import { flatten } from 'lodash';
 
 import { TimeEntry } from '../types';
 
-export const getUsernamesWhoHavePlayedOnDate = async (client: Client, date: string) => {
-  return await client.query(`
-    SELECT username FROM "pjflanagan/nyt_crossword_plus"."times" WHERE "date"='${date}';
-  `);
+export const getUsernamesWhoHavePlayedOnDate = async (client: Client, date: string): Promise<string[]> => {
+  const result = await client.query({
+    rowMode: 'array',
+    text: `SELECT username FROM "pjflanagan/nyt_crossword_plus"."times" WHERE "date"=DATE('${date}');`
+  });
+  return flatten(result.rows);
 }
 
 export const writeTimes = async (client: Client, entries: TimeEntry[]) => {
