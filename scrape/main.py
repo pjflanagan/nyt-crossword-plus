1e7de156a3015a917333a815231197c91d5d17e8
Peter Flanagan <pflanagan@grubhub.com>
2022-02-21 18:39:50 -0500

    have scraper write directly to db

--- scrape/main.py
+++ scrape/main.py
@@ -2,8 +2,17 @@ import requests
 import logging
 from bs4 import BeautifulSoup as soup
 import os
-import json
 import calendar
+import bitdotio
+
+DB_NAME = '"pjflanagan/nyt_crossword_plus"'
+
+def getUsernamesWhoHavePlayedOnDate(cur, date):
+    cur.execute('SELECT username FROM {DB_NAME}."times" WHERE "date"=DATE(\'{date}\');'.format(date=date))
+
+def writeTimes(cur, entries):
+    valuesFormatted = ",".join(list(map(lambda e: '(\'{username}\', \'{date}\', {time})'.format(username=e.username, date=e.date, time=e.time), entries)))
+    cur.execute('INSERT INTO {DB_NAME}."times" ("username", "date", "time") VALUES {values};'.format(values=valuesFormatted))
 
 def get_cookie(username, password):
     """Fetches a login cookie from the NYT api
@@ -15,6 +24,7 @@ def get_cookie(username, password):
     Raises: ValueError if cookie is not returned
     """
 
+    logging.info('Getting cookie')
     login_resp = requests.post(
         'https://myaccount.nytimes.com/svc/ios/v2/login',
         data={
@@ -32,7 +42,6 @@ def get_cookie(username, password):
             return cookie['cipheredValue']
     raise ValueError('NYT-S cookie not found')
 
-
 def scrape_leaderboard(cookie):
     """Scrapes the leaderboard for the user that the provided cookie corresponds to.
     Args:
@@ -41,11 +50,13 @@ def scrape_leaderboard(cookie):
     Returns: List all completed entries for the current day.
     """
 
+    logging.info('Loading leaderboard')
     url = 'https://www.nytimes.com/puzzles/leaderboards'
     response = requests.get(url, cookies={
         'NYT-S': cookie,
     })
     page = soup(response.content, features='html.parser')
+    
     solvers = page.find_all('div', class_='lbd-score')
     [_, month, day, year] = page.find('h3', class_='lbd-type__date').text.strip().split()
 
@@ -54,6 +65,7 @@ def scrape_leaderboard(cookie):
     month_string = f'{month_number:02d}'
     day_string = f'{int(day):02d}'
     timestamp = year + '-' + month_string + '-' + day_string
+    logging.info('Parsed timestamp')
 
     entries = []
     for solver in solvers:
@@ -76,6 +88,7 @@ def scrape_leaderboard(cookie):
         except:
             pass
 
+    logging.info('Parsed entries')
     return entries
 
 def save_entries(entries):
@@ -84,19 +97,39 @@ def save_entries(entries):
         entries (list(dict)): Entries to send to the backend
     """
 
-    url = ('https://nytcrosswordplus.flanny.app/api/times/write?k={api_key}').format(api_key=os.environ['API_KEY'])
-    request_body = {
-        'entries': entries,
-    }
-    serialized_body = json.dumps(request_body)
-    logging.info('Serialized payload: {payload}'.format(payload=request_body))
+    logging.info('Logging into db')
+    bitio_api_key = os.environ['BITIO_API_KEY']
+    db = bitdotio.bitdotio(bitio_api_key)
 
-    save_entries_response = requests.post(url, data=serialized_body)
+    with db.get_connection() as con:
+        cur = con.cursor()
 
-    if save_entries_response.ok:
-        logging.info('Received successful response: {response}'.format(response=save_entries_response.json()))
-    else:
-        logging.error('Received error response: {response}'.format(response=save_entries_response.json()))
+        logging.info('Loading previous entries')
+        prevUsernames = []
+        try:
+            prevUsernames = getUsernamesWhoHavePlayedOnDate(cur, entries[0].date)
+        except Exception as error:
+            logging.error('{error}'.format(error=error))
+            return
+
+        newEntries = []
+        if prevUsernames.length > 0:
+            logging.info('Filtering previous entries')
+            newEntries = filter(lambda e: e in prevUsernames, entries)
+        else:
+            logging.info('No previous entries to filter')
+        
+        if len(newEntries) == 0:
+            logging.info('No new entries')
+            return
+
+        logging.info('Writing new times')
+        try:
+            writeTimes(cur, newEntries)
+            
+        except Exception as error:
+            logging.error('{error}'.format(error=error))
+            return 
 
 def main(event, context):
     """Triggered from a message on a Cloud Pub/Sub topic.
